001bae7b37655a69e3390d615737083cccd0b596
danna-aa <danna@berkeley.edu>
2019-09-09 00:27:12 -0700

    submit W4D1 hw

---
+++ W4D1/graphnode.rb
@@ -0,0 +1,48 @@
+class GraphNode
+
+    attr_accessor :value, :neighbors
+
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+    def inspect
+        { "value"=>@value, "neighbors"=>@neighbors }.inspect
+    end
+
+    def bfs(starting_node, target_value)
+        queue = [starting_node]
+        visited = []
+        path = []
+        until queue.empty?
+            node = queue.shift
+            if node.value == target_value
+                return node
+            elsif visited.include?(node)
+                queue += []
+            else
+                visited << node
+                node.neighbors.each do |nod|
+                    queue << nod if !visited.include?(nod)
+                end
+            end
+        end
+        return nil
+    end
+
+end
+
+a = GraphNode.new('a')
+b = GraphNode.new('b')
+c = GraphNode.new('c')
+d = GraphNode.new('d')
+e = GraphNode.new('e')
+f = GraphNode.new('f')
+a.neighbors = [b, c, e]
+c.neighbors = [b, d]
+e.neighbors = [a]
+f.neighbors = [e]
+
+p a.bfs(a, "b")
+p a.bfs(a, "f")
